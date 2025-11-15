cab7e1a01abf5a743cca36ae308ca287657d1841
kaguangieliud56 <kaguangieliud56@gmail.com>
2025-11-15 22:58:00 +0300

    functions budgeting

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
+function calculateTax(amount){
+    const tax =  0.10;
+    return tax;
+}
 
+function convertToUpperCase(text){
+    return text.toUpperCase();
+}
 
+function findMaximum(num1, num2){
+    return num1 > num2 ? num1 : num2;
+}
 
+function isPalindrome(word){
+    const reversed = word.split('').reverse().join('');
+    return word === reversed;
+}
 
-
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+function calculateDiscountedPrice(originalPrice, discountPercentage){
+    const discountAmount = (discountPercentage / 100) * originalPrice;
+const discountedPrice = originalPrice - discountAmount;
+return discountedPrice;
+}
+console.log(calculateTax(200));
+console.log(convertToUpperCase("hello"));
+console.log(findMaximum(10, 20));
+console.log(isPalindrome("racecar"));
+console.log(calculateDiscountedPrice(100, 20));
